7aab25825a69411cf619b65a4205f0dcb4fd05f0
Ayman Sedqi <sedqiayman02@gmail.com>
2024-05-14 12:27:36 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+
+
+  return (
+    <>
+        <main className='relative w-full h-screen flex justify-center items-center bg-paleBlue'>
+          <img className='w-full absolute top-0 left-0 right-0' src="/pattern-background-desktop.svg" alt="" />
+
+
+        </main>
+    </>
+  )
+}
+
+export default App
